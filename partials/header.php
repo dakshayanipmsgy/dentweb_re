282ae83ce31a1cec91fc9601e12eba1054ba3977
dakshayanipmsgy <dakshayani.pmsgy@gmail.com>
2026-03-26 14:03:14 +0530

    Revert "Add Solar and Finance public page with admin-editable pricing & calculator"

--- partials/header.php
+++ partials/header.php
@@ -37,7 +37,6 @@ $global = $ws['global'] ?? [];
           <a href="/innovation-tech.html" class="nav-link" role="menuitem">Innovation &amp; Tech</a>
           <a href="/blog/index.php" class="nav-link" role="menuitem">Blog &amp; Insights</a>
           <a href="/calculator.html" class="nav-link" role="menuitem">Solar Calculator</a>
-          <a href="/solar-and-finance.php" class="nav-link" role="menuitem">Solar &amp; Finance</a>
           <a href="/policies.html" class="nav-link" role="menuitem">Policies &amp; Compliance</a>
         </div>
       </div>
@@ -87,7 +86,6 @@ $global = $ws['global'] ?? [];
       <a href="/innovation-tech.html">Innovation &amp; Tech</a>
       <a href="/blog/index.php">Blog &amp; Insights</a>
       <a href="/calculator.html">Solar Calculator</a>
-      <a href="/solar-and-finance.php">Solar &amp; Finance</a>
       <a href="/policies.html">Policies &amp; Compliance</a>
     </div>
     <div class="nav-mobile-divider" role="presentation"></div>
